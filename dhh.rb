2282fcee17fa142bd4b89064681ec26d9ad48836
Garrett Duncan <duncansports_17@hotmail.com>
2025-07-19 23:22:36 -0500

    Add initial implementation to display messages and current index

---
+++ dhh.rb
@@ -0,0 +1,5 @@
+5.times { puts "Hello, World!" } # display "Hello, World!" five times
+
+5.times { |i| puts "Current index: #{i}" } # display current index from 0 to 4
+
+# reducing line noise DHH-style
